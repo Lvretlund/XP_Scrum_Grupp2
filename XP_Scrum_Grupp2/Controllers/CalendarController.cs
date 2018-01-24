dae0faabe029fdcb4bb7274e6af74b22f1bf3b8b
Samerhadi1997 <samer@LAPTOP-68F0URJC>
2018-01-24 14:25:20 +0100

    Ny Meny

---
+++ XP_Scrum_Grupp2/Controllers/CalendarController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace XP_Scrum_Grupp2.Controllers
+{
+    public class CalendarController : Controller
+    {
+        // GET: Calendar
+        public ActionResult ShowCalendar()
+        {
+            return View();
+        }
+    }
+}
---
+++ XP_Scrum_Grupp2/Controllers/MeetingController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace XP_Scrum_Grupp2.Controllers
+{
+    public class MeetingController : Controller
+    {
+        // GET: Meeting
+        public ActionResult ShowMeeting()
+        {
+            return View();
+        }
+    }
+}
