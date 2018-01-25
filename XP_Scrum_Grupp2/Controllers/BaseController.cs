fe7b7a4a7e3670ad816329d1c7534c82d0a47531
EllenorMoshel <ellenor.moshel@hotmail.com>
2018-01-25 13:59:35 +0100

    Det går att posta på en vägg men funktionaliteten för filuppladdning och val av kategori finns ännu inte.

---
+++ XP_Scrum_Grupp2/Controllers/BaseController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using XP_Scrum_Grupp2.Models;
+
+namespace XP_Scrum_Grupp2.Controllers
+{
+    public class BaseController : Controller
+    {
+        protected ApplicationDbContext db = new ApplicationDbContext();
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing) db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
