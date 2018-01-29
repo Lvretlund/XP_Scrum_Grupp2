80c0288a4e919d65bfa64fdea3b684efb6f24505
Matilda Olsson <matilda.la.olsson@live.se>
2018-01-29 11:49:25 +0100

    Blog-stuff

--- XP_Scrum_Grupp2/Models/FormalBlog.cs
+++ XP_Scrum_Grupp2/Models/FormalBlog.cs
@@ -10,15 +10,17 @@ namespace XP_Scrum_Grupp2.Models
         public int Id { get; set; }
         public string Text { get; set; }
         public ApplicationUser Author { get; set; }
-        public Category CategoryItem { get; set; }
-
+        public ICollection<Category> Categories { get; set; }
         public  DateTime Date { get; set; }
-
         public byte[] File { get; set; }
+        public string ContentType { get;  set; }
+        public string Filename { get;  set; }
+        public ICollection<FormalBlog> FormalBlogs { get; set; }
+        //public FormalBlog NewFormalBlog { get; set; } = new FormalBlog();
+
 
         public virtual ApplicationUser User { get; set; }
         public virtual Category Category { get; set; }
-        public string ContentType { get;  set; }
-        public string Filename { get;  set; }
+        
     }
 }
