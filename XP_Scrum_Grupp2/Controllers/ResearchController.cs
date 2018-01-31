bb42ad3213e11f885706e1d94e94ddaaafbdcec4
EllenorMoshel <ellenor.moshel@hotmail.com>
2018-01-31 10:31:57 +0100

    Education blog admin

---
+++ XP_Scrum_Grupp2/Controllers/ResearchController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace XP_Scrum_Grupp2.Controllers
+{
+    public class ResearchController : Controller
+    {
+        // GET: Research
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
