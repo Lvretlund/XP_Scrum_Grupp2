17778fe89335db51fcd91e925d4b57cc4877eab9
marieedliden <31924575+marieedliden@users.noreply.github.com>
2018-02-12 09:53:49 +0100

    meeting invited fix + calender show invitees

--- XP_Scrum_Grupp2/Models/Meeting.cs
+++ XP_Scrum_Grupp2/Models/Meeting.cs
@@ -8,19 +8,20 @@ namespace XP_Scrum_Grupp2.Models
 {
     public class Meeting
     {
-
+     
         public int Id { get; set; }
         public string Title { get; set; }
         public ApplicationUser Creator { get; set; }
-        public ICollection<ApplicationUser> Invited { get; set; }
+        public string CreatorId { get; set; }
         public double Minutes { get; set; }
         public DateTime Start { get; set; }
         public DateTime End { get; set; }
         public ICollection<DateTime> Times { get; set; }
         public bool AllDay { get; set; }
 
-        public ICollection<ApplicationUser> User { get; set; }
+        public ICollection<MeetingInvited> MeetingInvited { get; set; }
         public ApplicationUser UserNotis { get; set; }
+        public ICollection<ApplicationUser> User { get; set; }
         
     }
 }
